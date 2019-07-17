1c4c92613e4b105e463008287ddf9ca256d61c9f
Haoxing Liang <edentliang@gmail.com>
2019-07-17 15:57:12 -0400

    added update client sql

--- routes/update_client.js
+++ routes/update_client.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var router = express.Router();
+var connection = require('../Database/DB_Connection.js');
 
 router.get("/update_client", function(req, res) {
    res.render("update_client");
@@ -11,17 +12,31 @@ router.post("/update_client", function(req, res) {
    var phone = req.body.Phone;
    var address = req.body.Address;
 
-   var sql = "UPDATE CLIENT SET Budget = ?";
-   connection.query(sql, [budget, phone, address], function (err, result) {
-      if (err) throw err;
-      console.log("Update Budget of client!");
-   });
+   if (budget) {
+      var sql = "UPDATE CLIENT SET Budget = ? WHERE CLIENT_ID = ?" ;
+      connection.query(sql, [budget, clientId], function (err, result) {
+         if (err) throw err;
+         console.log("Update Budget of client!");
+      });
+   }
+   
+   if (phone) {
+      var sql = "UPDATE CLIENT SET Phone_Num = ? WHERE CLIENT_ID = ?";
+      connection.query(sql, [phone, clientId], function (err, result) {
+         if (err) throw err;
+         console.log("Update phone num of client!");
+      });
+   }
 
-   var sql = "UPDATE CLIENT SET Budget = ?";
-   connection.query(sql, [budget, phone, address], function (err, result) {
-      if (err) throw err;
-      console.log("Update Budget of client!");
-   });
+   if (address) {
+      var sql = "UPDATE CLIENT SET Address = ? WHERE CLIENT_ID = ?";
+      connection.query(sql, [address, clientId], function (err, result) {
+         if (err) throw err;
+         console.log("Update address of client!");
+      });
+   }
+
+
 });
 
 module.exports = router;
