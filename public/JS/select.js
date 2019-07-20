edae052c0adcbd087ef98561b376d2f4dd12dbbd
Shibo(Johnny) Meng <shibomeng@yahoo.com>
2019-07-20 15:11:04 -0400

    test

--- public/JS/select.js
+++ public/JS/select.js
@@ -5,23 +5,20 @@ var flower = document.querySelector("#flower");
 var menu_div = document.querySelector("#menu_div");
 var flower_div = document.querySelector("#flower_div");
 
-menu_div.addEventListener("click", function() {
-    console.log("you");
+Menu.addEventListener("click", function() {
     for (i = 0; i < Menu.length; i++) {
         if (Menu[i].selected) {
             MenuQuantity.style.display = "block";
-            console.log("fuck");
         } else if (i == Menu.length - 1) {
             MenuQuantity.style.display = "none";
         }
     }
 });
 
-flower_div.addEventListener("click", function() {
+flower.addEventListener("click", function() {
     for (i = 0; i < flower.length; i++) {
         if (flower[i].selected) {
             FWQuantity.style.display = "block";
-            console.log("fuck");
         } else if (i == flower.length - 1) {
             FWQuantity.style.display = "none";
         }
