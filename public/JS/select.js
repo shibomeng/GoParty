f1281addab43647fed6dcbaa82cd7b7485d7ef07
Shibo(Johnny) Meng <shibomeng@yahoo.com>
2019-07-20 18:54:14 -0400

    r

--- public/JS/select.js
+++ public/JS/select.js
@@ -1,27 +1,24 @@
 var MenuQuantity = document.querySelector("#MenuQuantity");
 var FWQuantity = document.querySelector("#FWQuantity");
-var Menu = document.querySelector("#Menu").options;
-var flower = document.querySelector("#flower").options;
+var Menu = document.querySelector("#Menu");
+var flower = document.querySelector("#flower");
 var menu_div = document.querySelector("#menu_div");
 var flower_div = document.querySelector("#flower_div");
 
-Menu.addEventListener("click", function() {
-    console.log(Menu.length);
-    for (i = 0; i < Menu.length; i++) {
-        if (Menu[i].selected) {
-            MenuQuantity.style.display = "block";
-        } else if (i == Menu.length - 1) {
-            MenuQuantity.style.display = "none";
-        }
-    }
-});
+// Menu.addEventListener("focus", function() {
+//     MenuQuantity.style.display = "block";
+// });
 
-flower.addEventListener("click", function() {
-    for (i = 0; i < flower.length; i++) {
-        if (flower[i].selected) {
-            FWQuantity.style.display = "block";
-        } else if (i == flower.length - 1) {
-            FWQuantity.style.display = "none";
-        }
-    }
-});
+// Menu.addEventListener("focusout", function() {
+//     MenuQuantity.style.display = "none";
+// });
+
+// flower.addEventListener("click", function() {
+//     for (i = 0; i < flower.length; i++) {
+//         if (flower[i].selected) {
+//             FWQuantity.style.display = "block";
+//         } else if (i == flower.length - 1) {
+//             FWQuantity.style.display = "none";
+//         }
+//     }
+// });
