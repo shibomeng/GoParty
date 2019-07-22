ca65476227e0df6f3bc30e03ff66ce61b2f9a6f6
Haoxing Liang <edentliang@gmail.com>
2019-07-21 22:18:15 -0400

    added total price

--- routes/new_order.js
+++ routes/new_order.js
@@ -23,11 +23,12 @@ router.get("/new_order", function(req, res) {
 });
 
 router.post("/new_order", function(req, res) {
-    var clientID = req.body.ClientID;
-    var orderID = req.body.OrderID;
-    var numOfInvitees = req.body.invitee;
-    var event = req.body.Event;
-    var venue = req.body.Venue;
+   var clientID = req.body.ClientID;
+   var orderID = req.body.OrderID;
+   var numOfInvitees = req.body.invitee;
+   var event = req.body.Event;
+   var venue = req.body.Venue;
+   var total = 0;
 
    var sql = "INSERT INTO ORDER_INFO (ORDER_INFO_ID, Client_ID, Num_Of_Invitees, Location,Event_Type) VALUES (?, ?, ?, ?, ?)";
    connection.query(sql, [orderID, clientID, numOfInvitees, venue, event], function (err, result) {
@@ -39,33 +40,40 @@ router.post("/new_order", function(req, res) {
       var id;
       var menu = req.body.Menu;
       var quantity = req.body.MenuQuantity;
-      connection.query("SELECT Menu_ID FROM MENU_ITEM WHERE Name = ?", [menu], function(err, id){
+      connection.query("SELECT Menu_ID, Price FROM MENU_ITEM WHERE Name = ?", [menu], function(err, result){
          if (err) throw err;
-         connection.query("INSERT INTO CONSIST_MENU (Menu_ID, ORDER_INFO_ID,Client_ID,Quantity) VALUES (?,?,?,?)", [id[0].Menu_ID, orderID, clientID, quantity],
+         connection.query("INSERT INTO CONSIST_MENU (Menu_ID, ORDER_INFO_ID,Client_ID,Menu_Quantity) VALUES (?,?,?,?)", [result[0].Menu_ID, orderID, clientID, quantity],
             function (err, result) {
                if (err) throw err;
-            });
+         });
+         total += parseInt(quantity) * parseInt(result[0].Price);
       });
       
 
       var decor = req.body.FW;
       quantity = req.body.FWQuantity;
-      connection.query("SELECT Decor_ID FROM DECOR_ITEM WHERE Name = ?", [decor], function (err, id) {
+      connection.query("SELECT Decor_ID, Price FROM DECOR_ITEM WHERE Name = ?", [decor], function (err, result) {
          if (err) throw err;
-         connection.query("INSERT INTO CONSIST_DECOR (Decor_ID, ORDER_INFO_ID,Client_ID,Quantity) VALUES (?,?,?,?)", [id[0].Decor_ID, orderID, clientID, quantity],
+         connection.query("INSERT INTO CONSIST_DECOR (Decor_ID, ORDER_INFO_ID,Client_ID,Decor_Quantity) VALUES (?,?,?,?)", [result[0].Decor_ID, orderID, clientID, quantity],
             function (err, result) {
                if (err) throw err;
-            });
+         });
+         total += parseInt(quantity) * parseInt(result[0].Price);
       });
+
      
 
       var entertainment = req.body.ME;
-      connection.query("SELECT Entertainment_ID FROM ENTERTAINMENT_ITEM WHERE Name = ?", [entertainment], function (err, id) {
+      connection.query("SELECT Entertainment_ID, Price FROM ENTERTAINMENT_ITEM WHERE Name = ?", [entertainment], function (err, result) {
          if (err) throw err;
-         connection.query("INSERT INTO CONSIST_ENTERTAINMENT (Entertainment_ID, ORDER_INFO_ID,Client_ID) VALUES (?,?,?)", [id[0].Entertainment_ID, orderID, clientID],
+         connection.query("INSERT INTO CONSIST_ENTERTAINMENT (Entertainment_ID, ORDER_INFO_ID,Client_ID) VALUES (?,?,?)", [result[0].Entertainment_ID, orderID, clientID],
             function (err, result) {
                if (err) throw err;
-            });
+         });
+         total += parseInt(result[0].Price);
+         connection.query("UPDATE ORDER_INFO SET Total_Price = ? WHERE ORDER_INFO_ID = ?", [total, orderID], function (err, result) {
+            if (err) throw err;
+         });
       });
 
       req.flash("success", "Successfully Added New Order!");
