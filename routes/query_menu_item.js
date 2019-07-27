95d4300132634b513674d1ed4caae4f75adfa703
Haoxing Liang <edentliang@gmail.com>
2019-07-27 17:46:37 -0400

    added numof Orders

--- routes/query_menu_item.js
+++ routes/query_menu_item.js
@@ -23,13 +23,14 @@ router.get("/query_menu_item", function (req, res) {
 
 router.post("/query_menu_item", function (req, res) {
     var name = req.body.Name;
-    var sql = " SELECT * FROM  SUPPLIER \
+    var sql;
+    var Menu_sql = "SELECT * FROM SUPPLIER \
                 natural join SUPPLY_MENU \
                 natural join MENU_ITEM \
                 where MENU_ITEM.Name = ? and MENU_ITEM.Name = SUPPLY_MENU.Menu_Name \
-                and SUPPLIER.Supplier_Name = SUPPLY_MENU.Supplier_Name";
-    
-    var Decor_sql = "   SELECT * FROM  SUPPLIER \
+                and SUPPLIER.Supplier_Name = SUPPLY_MENU.Supplier_Name ";
+
+    var Decor_sql = "   SELECT * FROM SUPPLIER \
                         natural join SUPPLY_DECOR \
                         natural join DECOR_ITEM \
                         where DECOR_ITEM.Name = ? and DECOR_ITEM.Name = SUPPLY_DECOR.Decor_Name \
@@ -39,10 +40,14 @@ router.post("/query_menu_item", function (req, res) {
                         natural join SUPPLY_ENTERTAINMENT \
                         natural join ENTERTAINMENT_ITEM \
                         where ENTERTAINMENT_ITEM.Name = ? and ENTERTAINMENT_ITEM.Name = SUPPLY_ENTERTAINMENT.Entertainment_Name \
-                        and SUPPLIER.Supplier_Name = SUPPLY_ENTERTAINMENT.Supplier_Name"; 
+                        and SUPPLIER.Supplier_Name = SUPPLY_ENTERTAINMENT.Supplier_Name "; 
 
-    connection.query(sql, [name], function (err, Minfo){
+    var result1 = 0;
+    var result2 = 0;
+    var result3 = 0;
+    connection.query(Menu_sql, [name], function (err, Minfo){
         if (err) throw err;
+        // console.log(Minfo);
         connection.query(Decor_sql, [name], function (err, Dinfo){
             if (err) throw err;
             connection.query(Entertainment_sql, [name], function (err, Einfo){
@@ -50,10 +55,44 @@ router.post("/query_menu_item", function (req, res) {
                 var info;
                 if (Minfo.length != 0) {
                     info = Minfo;
+                    sql = "SELECT MENU_NAME, count(*) AS NumOf_Orders \
+                    FROM CONSIST_MENU \
+                    where  MENU_NAME = ?"
+                    connection.query(sql, [name], function (err, result) {
+                        if (err) throw err;
+                        if (result.length != 0) {
+                            result1 = result[0]['NumOf_Orders'];
+                        }
+                        info[0]['Numof_Orders'] = result1;
+                    });
+                   
                 } else if (Dinfo.length != 0) {
                     info = Dinfo;
+                    
+                    sql = "SELECT DECOR_NAME, count(*) AS NumOf_Orders \
+                        FROM CONSIST_DECOR \
+                        where  DECOR_NAME = ?"
+                    connection.query(sql, [name], function (err, result) {
+                        if (err) throw err;
+                        if (result.length != 0) {
+                            result2 = result[0]['NumOf_Orders'];
+                        }
+                        info[0]['Numof_Orders'] = result2;
+                    });
+                    
                 } else if (Einfo.length != 0) {
                     info = Einfo;
+                    sql = "SELECT Entertainment_Name, count(*) AS NumOf_Orders \
+                            FROM CONSIST_ENTERTAINMENT \
+                            where  Entertainment_Name = ?"
+                    connection.query(sql, [name], function (err, result) {
+                        if (err) throw err;
+                        if (result.length != 0) {
+                            result3 = result[0]['NumOf_Orders'];
+                        }
+                        info[0]['Numof_Orders'] = result3;
+                    });
+                    
                 } 
                 var menu;
                 connection.query("SELECT NAME FROM MENU_ITEM", function (err, result) {
@@ -69,7 +108,7 @@ router.post("/query_menu_item", function (req, res) {
                 connection.query("SELECT NAME FROM ENTERTAINMENT_ITEM", function (err, result) {
                     if (err) throw err;
                     entertainment = result;
-                    delete info[0].Name;
+                    // delete info[0].Name;
                     res.render("query_menu_item", { info: info, menu: menu, decor: decor, entertainment: entertainment })
                 });
             })
